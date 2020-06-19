3b7e6fcb2be7a0778ed89f5a6ae26f5611d0b9da
CSCTITest <CSCTITest@IDEXRelease>
2020-06-19 02:26:44 +0000

    Add project files.

---
+++ 12345678912345678912345678912345678912345678912345/Class1dddddddddddddddddd.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _12345678912345678912345678912345678912345678912345
+{
+    class Class1dddddddddddddddddd
+    {
+    }
+}
